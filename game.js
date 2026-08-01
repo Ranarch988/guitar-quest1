437ca792565a4004bebdb206e0e08d36ea3c4b08
Ranarch988 <4pgtz9kvmj@privaterelay.appleid.com>
2026-08-01 17:07:41 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,56 @@
-console.log("Guitar Quest başladı!");
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 500,
+    backgroundColor: "#1b2440",
+    scene: {
+        create: create
+    }
+};
+
+new Phaser.Game(config);
+
+function create() {
+
+    // Başlık
+    this.add.text(400, 80, "GUITAR QUEST", {
+        fontSize: "48px",
+        color: "#ffffff",
+        fontStyle: "bold"
+    }).setOrigin(0.5);
+
+    // Alt başlık
+    this.add.text(400, 140, "Collect 10 Guitars!", {
+        fontSize: "22px",
+        color: "#dddddd"
+    }).setOrigin(0.5);
+
+    // PLAY butonu
+    const play = this.add.rectangle(400, 260, 220, 70, 0x2ecc71)
+        .setInteractive({ useHandCursor: true });
+
+    this.add.text(400, 260, "PLAY", {
+        fontSize: "32px",
+        color: "#ffffff",
+        fontStyle: "bold"
+    }).setOrigin(0.5);
+
+    // Alt yazı
+    this.add.text(400, 470, "Made with nihaofineshyt", {
+        fontSize: "16px",
+        color: "#aaaaaa"
+    }).setOrigin(0.5);
+
+    // Butona basınca
+    play.on("pointerdown", () => {
+        this.cameras.main.flash(300);
+
+        this.time.delayedCall(350, () => {
+            this.add.text(400, 360, "Coming Soon...", {
+                fontSize: "28px",
+                color: "#ffff66"
+            }).setOrigin(0.5);
+        });
+    });
+
+}
